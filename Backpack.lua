ab6c292568db8a85b2e5d40fb4ade328cf34a768
Basictality <www.basictality@gmail.con>
2015-02-24 16:23:47 -0500

    Update Backpack.lua

--- Backpack.lua
+++ Backpack.lua
@@ -363,7 +363,7 @@ we.C0=CFrame.new(0,0.3,0)
 r65.Head.Transparency=1
 
 Heart=Instance.new("Part",a)
-Heart.Size = Vector3.new(1.5,1.5,1.5)
+Heart.Size = Vector3.new(1.2,1.2,1.2)
 Heartw=Instance.new("Weld",Heart)
 Heartw.Part0=r65.Torso
 Heartw.Part1=Heart
